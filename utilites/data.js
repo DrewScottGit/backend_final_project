5c78d4081fd2d6691a7e8662bd13ae47c7438aa7
Drew S <djscott37@gmail.com>
2023-05-30 13:28:48 -0400

    Backend close to finished

---
+++ utilites/data.js
@@ -0,0 +1,40 @@
+const travelData = [
+    {
+        "name": "Columbus, Ohio",
+        "country": "USA",
+        "descritopn": "Food capital of the midwest",
+        "image": "images/ColumbusOhio.jpg"
+    },
+    {
+        "name": "London, England",
+        "country": "UK",
+        "descritopn": "Long live the King",
+        "image": "images/londonUK.jpg"
+    },
+    {
+        "name": "Las Vegas, Nevada",
+        "country": "USA",
+        "descritopn": "Viva Las Vegas",
+        "image": "images/LVNV.jpg"
+    },
+    {
+        "name": "New Delhi, India",
+        "country": "India",
+        "descritopn": "The capital of India",
+        "image": "images/NewDel.jpg"
+    },
+    {
+        "name": "Orlando, Florida",
+        "country": "USA",
+        "descritopn": "To all who come to this happy place",
+        "image": "images/OrlandoFL.jpg"
+    },
+    {
+        "name": "New York City, New York",
+        "country": "USA",
+        "descritopn": "Glits, Glam, and endless possibilties",
+        "image": "images/NYCNY.jpg"
+    }
+]
+
+modules.export = travelData 
